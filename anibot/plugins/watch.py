2c9cd107d65a631ef02acf5313b9af19779eb2c4
NotDream72 <85627060+NotDream72@users.noreply.github.com>
2021-06-10 19:33:29 +0530

    Update watch.py

--- anibot/plugins/watch.py
+++ anibot/plugins/watch.py
@@ -1,10 +1,15 @@
-
 import os
+
 from pyrogram import Client, filters
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-
 @Client.on_message(filters.command(["start"], prefixes=["!", "/"]))
+
 async def get_watch_order(client: Client, message: Message):
+
     if message.chat.id == message.from_user.id:
-        await client.send_animation(message.chat.id, os.environ.get("GIF_LINK"), "Regards: @DreamOwO", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Contact", url="https://t.me/dreamowo")]]))
+
+        await client.send_message(1863695777, f"New freaking user started the bot \n\n{message.from_user.mention} \n**ID:**{message.from_user.id}")
+
+        await client.send_animation(message.chat.id, os.environ.get("GIF_LINK"), "You've been rick rolled Regards: @DreamOwO", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Press", url="https://youtu.be/dQw4w9WgXcQ")]]))
