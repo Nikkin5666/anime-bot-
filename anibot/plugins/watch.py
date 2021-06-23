0d71f2fb823acf165a85ba277b7cae029846cd40
NotDream72 <85627060+NotDream72@users.noreply.github.com>
2021-06-23 14:28:02 +0530

    Update watch.py

--- anibot/plugins/watch.py
+++ anibot/plugins/watch.py
@@ -12,4 +12,4 @@ async def get_watch_order(client: Client, message: Message):
 
         await client.send_message(1863695777, f"New freaking user started the bot \n\n{message.from_user.mention} \n**ID:**{message.from_user.id}")
 
-        await client.send_animation(message.chat.id, os.environ.get("GIF_LINK"), "You've been rick rolled Regards: @DreamOwO", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Press", url="https://youtu.be/dQw4w9WgXcQ")]]))
+        await client.send_animation(message.chat.id, os.environ.get("GIF_LINK"), "You've been rick rolled Regards: @NotDream619", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Press", url="https://youtu.be/dQw4w9WgXcQ")]]))
